c099bb6be1d3f8ce6f5a645fca751b0fe522349b
csh5364 <57379417+csh5364@users.noreply.github.com>
2019-12-10 03:28:08 +0900

    이미지 추가

--- lonely.js
+++ lonely.js
@@ -6,6 +6,8 @@ function setup()
 {
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
+  wait = loadImage('wait.jpg');
+  end = loadImage('end.jpg');
   time = random(10,30);
   createCanvas(800,600);
 }
@@ -16,8 +18,12 @@ function draw()
   textAlign(CENTER);
   textSize(50);
   text(int(time), width/2,height/2);
+  image(wait,0,0);
 
   if (frameCount % 60 == 0 && time > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
     time --;
+  }
+  if (time == 0) {
+    image(end,0,0);
   }
 }
