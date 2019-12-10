aba453ae95082e15b4bce87cd84b3d5ccdf69bc9
csh5364 <57379417+csh5364@users.noreply.github.com>
2019-12-10 10:34:13 +0900

    Update lonely.js

--- lonely.js
+++ lonely.js
@@ -15,16 +15,15 @@ function setup()
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
   time = random(10,30);
   createCanvas(800,600);
+  textAlign(CENTER);
+  textSize(50);
+  text(int(time), width/2,height/2);
+  image(wait,0,0);
 }
 
 function draw()
 {
   background(255);
-  textAlign(CENTER);
-  textSize(50);
-  text(int(time), width/2,height/2);
-  image(wait,0,0);
-
   if (frameCount % 60 == 0 && time > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
     time --;
   }
