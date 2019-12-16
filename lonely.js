f57f5528a9c8155d5073959a074d7c79348aab9f
csh5364 <57379417+csh5364@users.noreply.github.com>
2019-12-17 01:11:24 +0900

    그래야하나요 왜죠

--- lonely.js
+++ lonely.js
@@ -1,19 +1,25 @@
 let canvasWidth =800;
 let canvasHeight=420;
-let wait;
-let end;
+let wait_1;
+let wait_2;
+let end_1;
+let end_2;
 let time;
 
 function preload() {
-  wait = loadImage('wait.jpg');
-  end = loadImage('end.jpg');
+  image1 = loadImage('1.png');
+  image2 = loadImage('2.png');
+  image3 = loadImage('3.png');
+  image4 = loadImage('4.png');
+
+  let i = random(1,2);
 }
 
 function setup()
 {
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
-  time = random(1, 5);
+  time = 5//random(1, 5);
   createCanvas(800,420);
 }
 
@@ -28,9 +34,20 @@ function draw()
   if (frameCount % 60 == 0 && time > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
     time --;
   }
+
+  if (i == 1){
   if (time == 0) {
-    image(end,0,0);
+    image(image1,0,0);
   }
   else {
-    image(wait,0,0);}
+    image(image3,0,0);}
+  }
+
+  if (i == 2){
+  if (time == 0) {
+    image(image2,0,0);
+  }
+  else {
+    image(image4,0,0);}
+  }
 }
